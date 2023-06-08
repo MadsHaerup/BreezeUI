7d3ba690eb6df09c2ee1fafc0c9f6dc79c1f257b
Mads Hærup <madshaerup@outlook.com>
2023-06-08 14:54:57 +0200

    extend tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ export default {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			gridTemplateColumns: {
+				autofit: 'repeat(auto-fit, minmax(200px, 1fr))',
+			},
+		},
 	},
 	plugins: [],
 };
