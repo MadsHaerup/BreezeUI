c6e0e487c0e127810213fcd03398e4fa54f4fac7
Mads Hærup <madshaerup@outlook.com>
2023-06-08 15:59:29 +0200

    remove unused tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -14,21 +14,21 @@ function App() {
 			<Widget.Item />
 			<button onClick={() => toggleTheme()}>Toggle</button> */}
 			<Widget.Board>
-				<Widget.Item tailwindCSS="sm:col-span-2 sm:row-span-2 bg-gray-200">Item 1</Widget.Item>
-				<Widget.Item tailwindCSS="bg-gray-200">Item 2</Widget.Item>
-				<Widget.Item tailwindCSS="bg-gray-200">Item 3</Widget.Item>
-				<Widget.Item tailwindCSS="bg-gray-200">Item 4</Widget.Item>
-				<Widget.Item tailwindCSS="bg-gray-200">Item 5</Widget.Item>
-				<Widget.Item tailwindCSS="sm:col-span-2 bg-gray-200">Item 6</Widget.Item>
-				<Widget.Item tailwindCSS="sm:col-span-2 sm:row-span-2 bg-gray-200">Item 7</Widget.Item>
-				<Widget.Item tailwindCSS="bg-gray-200">Item 8</Widget.Item>
-				<Widget.Item tailwindCSS="bg-gray-200">Item 9</Widget.Item>
-				<Widget.Item tailwindCSS="bg-gray-200">Item 10</Widget.Item>
-				<Widget.Item tailwindCSS="bg-gray-200">Item 11</Widget.Item>
-				<Widget.Item tailwindCSS="bg-gray-200">Item 12</Widget.Item>
-				<Widget.Item tailwindCSS="sm:col-span-3 bg-gray-200">Item 13</Widget.Item>
-				<Widget.Item tailwindCSS="bg-gray-200">Item 14</Widget.Item>
-				<Widget.Item tailwindCSS="bg-gray-200">Item 15</Widget.Item>
+				<Widget.Item tailwindCSS="sm:col-span-2 sm:row-span-2">Item 1</Widget.Item>
+				<Widget.Item>Item 2</Widget.Item>
+				<Widget.Item>Item 3</Widget.Item>
+				<Widget.Item>Item 4</Widget.Item>
+				<Widget.Item>Item 5</Widget.Item>
+				<Widget.Item tailwindCSS="sm:col-span-2">Item 6</Widget.Item>
+				<Widget.Item tailwindCSS="sm:col-span-2 sm:row-span-2">Item 7</Widget.Item>
+				<Widget.Item>Item 8</Widget.Item>
+				<Widget.Item>Item 9</Widget.Item>
+				<Widget.Item>Item 10</Widget.Item>
+				<Widget.Item>Item 11</Widget.Item>
+				<Widget.Item>Item 12</Widget.Item>
+				<Widget.Item tailwindCSS="sm:col-span-3">Item 13</Widget.Item>
+				<Widget.Item>Item 14</Widget.Item>
+				<Widget.Item>Item 15</Widget.Item>
 			</Widget.Board>
 		</main>
 	);
--- src/stories/WidgetBoard.stories.tsx
+++ src/stories/WidgetBoard.stories.tsx
@@ -17,7 +17,7 @@ type Story = StoryObj<typeof meta>;
 export const OneItem: Story = {
 	render: args => <Widget.Board>{args.children}</Widget.Board>,
 	args: {
-		children: <Widget.Item tailwindCSS="bg-gray-200">Item</Widget.Item>,
+		children: <Widget.Item>Item</Widget.Item>,
 	},
 };
 
@@ -26,21 +26,21 @@ export const ManyItems: Story = {
 	args: {
 		children: [
 			<>
-				<Widget.Item tailwindCSS="sm:col-span-2 sm:row-span-2 bg-gray-200">Item 1</Widget.Item>
-				<Widget.Item tailwindCSS="bg-gray-200">Item 2</Widget.Item>
-				<Widget.Item tailwindCSS="bg-gray-200">Item 3</Widget.Item>
-				<Widget.Item tailwindCSS="bg-gray-200">Item 4</Widget.Item>
-				<Widget.Item tailwindCSS="bg-gray-200">Item 5</Widget.Item>
-				<Widget.Item tailwindCSS="sm:col-span-2 bg-gray-200">Item 6</Widget.Item>
-				<Widget.Item tailwindCSS="sm:col-span-2 sm:row-span-2 bg-gray-200">Item 7</Widget.Item>
-				<Widget.Item tailwindCSS="bg-gray-200">Item 8</Widget.Item>
-				<Widget.Item tailwindCSS="bg-gray-200">Item 9</Widget.Item>
-				<Widget.Item tailwindCSS="bg-gray-200">Item 10</Widget.Item>
-				<Widget.Item tailwindCSS="bg-gray-200">Item 11</Widget.Item>
-				<Widget.Item tailwindCSS="bg-gray-200">Item 12</Widget.Item>
-				<Widget.Item tailwindCSS="sm:col-span-3 bg-gray-200">Item 13</Widget.Item>
-				<Widget.Item tailwindCSS="bg-gray-200">Item 14</Widget.Item>
-				<Widget.Item tailwindCSS="bg-gray-200">Item 15</Widget.Item>
+				<Widget.Item tailwindCSS="sm:col-span-2 sm:row-span-2">Item 1</Widget.Item>
+				<Widget.Item>Item 2</Widget.Item>
+				<Widget.Item>Item 3</Widget.Item>
+				<Widget.Item>Item 4</Widget.Item>
+				<Widget.Item>Item 5</Widget.Item>
+				<Widget.Item tailwindCSS="sm:col-span-2">Item 6</Widget.Item>
+				<Widget.Item tailwindCSS="sm:col-span-2 sm:row-span-2">Item 7</Widget.Item>
+				<Widget.Item>Item 8</Widget.Item>
+				<Widget.Item>Item 9</Widget.Item>
+				<Widget.Item>Item 10</Widget.Item>
+				<Widget.Item>Item 11</Widget.Item>
+				<Widget.Item>Item 12</Widget.Item>
+				<Widget.Item tailwindCSS="sm:col-span-3">Item 13</Widget.Item>
+				<Widget.Item>Item 14</Widget.Item>
+				<Widget.Item>Item 15</Widget.Item>
 			</>,
 		],
 	},
